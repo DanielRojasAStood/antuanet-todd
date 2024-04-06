f192eca83f81c2100fd6712656068605e416df91
Daniel Rojas <danielrojas243@gmail.com>
2024-04-05 20:58:16 -0500

    section 2

--- front-page.php
+++ front-page.php
@@ -16,10 +16,48 @@
     <img class="section-1__img-bottom" src="<?php echo IMG_BASE . 'separador-one.webp' ?>" alt="">
   </section>
 
-  <div style="height: 1000px"></div>
+  <section class="section-2">
+    <img class="section-2__bckg" src="<?php echo IMG_BASE . 'bg-two.webp' ?>" alt="">
+    <div class="section-2__copy">
+      <p class="title-26 m-b8">NUESTRA BODA</p>
+      <img class="m-b40" src="<?php echo IMG_BASE . 'text-our-wedding.svg' ?>" alt="">
+      <p class="title-24 m-b13">CEREMONY</p>
+      <p class="title-30">March 8th, 2025</p>
+      <p class="title-30 m-b13" style="line-height: normal">Baluarte San Ignacio</p>
+      <p class="title-32 m-b13" style="">7:00 P.M.</p>
+      <p class="title-24 m-b20">RECEPTION</p>
+      <p class="title-30 m-b40" style="line-height: normal">
+      Hotel Charleston <br> Santa Teresa
+      </p>
+      <div class="grid">
+        <button type="button" class="m-auto button button--transparent" data-open-modal="modal-1" > DRESS CODE </button>
+        <button type="button" class="m-auto button button--transparent" data-open-modal="modal-2" > GIFTS </button>
+      </div>
+    </div>
+  </section>
+
+  <section class="section-2 section-reverse">
+    <div class="section-2__copy">
+      <p class="title-26 m-b8">FIESTA BIENVENIDA</p>
+      <img class="m-b40" src="<?php echo IMG_BASE . 'text-phantom-party.svg' ?>" alt="">
+      <p class="title-30">March 7th, 2025</p>
+      <p class="title-32 m-b13" style="">5:00 P.M.</p>
+      <p class="title-30 m-b40" style="line-height: normal">
+      Barco Phantom
+      </p>
+      <div class="section-2__cta">
+        <button type="button" class="m-auto button button--transparent" data-open-modal="modal-3" > DRESS CODE </button>
+      </div>
+    </div>
+    <img class="section-2__bckg" src="<?php echo IMG_BASE . 'bg-three.webp' ?>" alt="">
+  </section>
+
+  <section>
+  </section>
+
   
     <!-- Modals -->
-    <!-- <section class="modal" style="display: none" data-modal="modal-1">
+    <section class="modal" style="display: none" data-modal="modal-1">
       <div class="modal__wrapper">
         <div class="modal__body">
           <button type="button" data-close-modal>
@@ -44,7 +82,7 @@
           />
         </div>
       </div>
-    </section> -->
+    </section>
     
   </main>
 </body>
